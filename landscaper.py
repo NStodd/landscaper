1d20dff9bd313aa7928f579686ad058cb9e5303d
Nate Stodd <nathan.stoddard@gmail.com>
2023-02-01 15:52:53 -0500

    implementing game functions

--- landscaper.py
+++ landscaper.py
@@ -13,6 +13,30 @@ money = 0
 tools = ["teeth"]
 day = 1
 
+# Game Functions
+
+      # morning function, to run at the start of each new day.
+def morning ():
+    print(f"It is day {day}, and you have ${money}.")
+    print("[1] Cut the Grass")
+    print("[2] Get a new tool")
+    print("[Q] Quit")
+    return input("What will you do today?")
+
+      # mow_the_grass function, when the user selects mowing the grass.
+def mow_the_grass ():
+      print("""Ok, good. A good work ethic is critical to success in this world.
+Your tools are:""")
+      for tool in tools:
+            print(f"[{tools.index(tool) + 1}] {tool}")
+      tool_choice = input("What will you use?")
+      print(tool_choice)
+      break
+
+def buy_new_tools ():
+      print("Welcome to the tool store!")
+      break
+
 # Initial Prompt
 print("""Welcome to your new Landscaping Business!
       There is a lot of grass to cut, and you're the one to do it.
@@ -21,8 +45,11 @@ print("""Welcome to your new Landscaping Business!
 
 # Game Loop
 while (True):
-    print(f"It is day {day}, and you have ${money}, what will you do?")
-    print("[1] Cut the Grass")
-    print("[2] Get a new tool")
-    print("[Q] Quit")
-    
+      choice = morning()
+      if choice == "Q" or choice == 'q':
+          print("Ok, Quitter.")
+          break
+      elif choice == "1":
+            mow_the_grass()
+      elif choice == "2":
+          buy_new_tools()
